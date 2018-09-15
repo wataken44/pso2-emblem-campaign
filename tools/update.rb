c2476ca64d3a0ade43e4d89f1cd2966faa2b8f1f
wataken44 <wataken44@gmail.com>
2018-09-15 06:52:48 +0000

    implement auto update

--- tools/update.rb
+++ tools/update.rb
@@ -20,8 +20,7 @@ def main()
     argv = opt.parse(ARGV)
     
     if auto
-        # implement later
-        # url = hogehoge
+        url = get_latest_url()
     else
         url = argv[0]
     end
@@ -30,12 +29,28 @@ def main()
     render_html(start, goals, auto, url)
 end
 
+def get_latest_url()
+    fp = open('http://pso2.jp/players/news/event/')
+    body = fp.read()
+    fp.close()
+
+    ptn = Regexp.compile('<a href="([^"]+)">.*王者の紋章[^<]*キャンペーン</span>')
+    links = body.scan(ptn)
+
+    if links.size > 0
+        return links[0][0]
+    end
+
+    return nil
+end
+
 def get_data(url)
     fp = open(url)
+    body = fp.read()
+    fp.close
 
     start_ptn = Regexp.compile('<span class="period-start"><span class="year">(\d+)</span>[^<]*<span class="mouth">(\d+)</span>[^<]*<span class="day">(\d+)</span>')
     goal_ptn = Regexp.compile("<tr><th>([^<]*)</th><th class=\"sub\">(.*)</th><td>([^<]*)</td></tr>")
-    body = fp.read()
     starts = body.scan(start_ptn)
 
     start = ""
@@ -45,8 +60,6 @@ def get_data(url)
         
     goals = body.scan(goal_ptn)
     
-    fp.close
-
     return [start, goals]
 end
 
